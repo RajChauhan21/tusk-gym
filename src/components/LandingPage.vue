3e314bb6aea6f1cebafda90d9579907b1b88af4f
RajChauhan21 <craj47577@gmail.com>
2026-03-15 12:57:40 +0530

    added wa svg

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,6 +2,7 @@
 import NavBar from './NavBar.vue';
 import bob_image from '../assets/tus_logo.png'
 import InfiniteCarousel from './InfiniteCarousel.vue';
+import wa from '../assets/wa_svg.svg'
 
 export default {
     components: {
@@ -15,7 +16,8 @@ export default {
             programs: 0,
             access: 0,
             started: false,
-            image: bob_image
+            image: bob_image,
+            waIcon:wa
         }
     },
     mounted() {
@@ -460,20 +462,24 @@ export default {
          justify-between px-6 md:px-12">
 
             <!-- Left -->
-            <p class="text-sm text-center md:text-left">
-                © 2026 Tushar Fitness Studio. All rights reserved.
-            </p>
+            <div class="text-center md:text-left text-sm">
+                <p> © 2026 Tushar Fitness Studio. All rights reserved.</p>
+
+                <p class="mt-1 text-gray-500">
+                    Created by
+                    <a href="https://www.linkedin.com/in/raj-chauhan-5bb540312/" target="_blank"
+                        class="text-yellow-500 hover:text-yellow-400 font-medium transition">
+                        Raj Chauhan
+                    </a>
+                </p>
+            </div>
 
             <!-- Right -->
-            <a href="https://wa.me/919768262139" target="_blank" class="mt-4 md:mt-0 flex gap-2
-           text-gray-400 hover:text-green-500
-           transition duration-300">
+            <a href="https://wa.me/919768262139" target="_blank" class="mt-4 md:mt-0 flex items-center gap-2
+           hover:text-green-500 transition duration-300">
 
-                <!-- WhatsApp SVG Icon -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M20.52 3.48A11.94 11.94 0 0 0 12.05 0C5.46 0 .11 5.35.11 11.94c0 2.1.55 4.16 1.6 5.97L0 24l6.27-1.64a11.93 11.93 0 0 0 5.78 1.48h.01c6.59 0 11.94-5.35 11.94-11.94 0-3.19-1.24-6.19-3.48-8.42zM12.06 21.8h-.01a9.87 9.87 0 0 1-5.03-1.37l-.36-.21-3.72.97.99-3.63-.23-.37a9.87 9.87 0 1 1 8.36 4.61zm5.46-7.39c-.3-.15-1.77-.87-2.05-.97-.27-.1-.47-.15-.67.15-.2.3-.77.97-.95 1.17-.17.2-.35.22-.65.07-.3-.15-1.27-.47-2.42-1.49-.9-.8-1.5-1.79-1.68-2.09-.17-.3-.02-.46.13-.61.13-.13.3-.35.45-.52.15-.17.2-.3.3-.5.1-.2.05-.37-.02-.52-.07-.15-.67-1.62-.92-2.22-.24-.58-.48-.5-.67-.5-.17-.01-.37-.01-.57-.01-.2 0-.52.07-.8.37-.27.3-1.05 1.03-1.05 2.5s1.08 2.88 1.23 3.08c.15.2 2.13 3.25 5.16 4.56.72.31 1.28.49 1.72.63.72.23 1.37.2 1.89.12.58-.09 1.77-.72 2.02-1.42.25-.7.25-1.3.17-1.42-.07-.12-.27-.2-.57-.35z" />
-                </svg>
+                <!-- WhatsApp Icon -->
+                <img :src="waIcon" class="w-6 h-6" />
 
                 <span class="text-sm">Chat on WhatsApp</span>
 
